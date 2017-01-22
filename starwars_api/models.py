a581c8489c41301dc1b467965eb0ec4b03d0187c
Jarold Wong <jaroldw@gmail.com>
2017-01-22 12:24:51 -0800

    Clean up comments

--- starwars_api/models.py
+++ starwars_api/models.py
@@ -21,7 +21,7 @@ class BaseModel(object):
         Returns an object of current Model requesting data to SWAPI using
         the api_client.
         """
-        
+
         return cls(getattr(api_client, "get_{}".format(cls.RESOURCE_NAME))(resource_id))
 
     @classmethod
@@ -31,39 +31,9 @@ class BaseModel(object):
         later in charge of performing requests to SWAPI for each of the
         pages while looping.
         """
-        
-        # this seems to work. not sure if this is valid or a "hack"
+
         return eval(cls.__name__ + 'QuerySet')()
 
-
-        # # this is what we had before
-        # if cls.RESOURCE_NAME == "people":
-        #     return PeopleQuerySet()
-
-        # if cls.RESOURCE_NAME == "films":
-        #     return FilmsQuerySet()
-
-#         # this is what I was experimenting with to compare a variety of results
-#         if cls.RESOURCE_NAME == "people":
-# #            return PeopleQuerySet()
-#             return eval(cls.__name__ + 'QuerySet')()
-
-#         if cls.RESOURCE_NAME == "films":
-# #             print(cls)
-# #             print(type(cls))
-# #             print(cls.__getattribute__)
-# #             print(cls.__class__)
-# #             print(type(FilmsQuerySet()))
-# #             print(cls.__name__)
-# #             print(eval(cls.__name__ + 'QuerySet')) # this may work.
-# # #            print(cls.get(cls))
-# # #            print(cls.all()) # infinite recursion of sorts? times out
-# #             print('-'*20)
-# #             print(dir(cls))
-# # #            return FilmsQuerySet() # this is what works right now.
-#             return eval(cls.__name__ + 'QuerySet')()
-# #            return cls._get_page_data()
-# #            return cls(getattr(api_client, "get_{}".format(cls.RESOURCE_NAME)))
 
 class People(BaseModel):
     """Representing a single person"""
@@ -105,7 +75,7 @@ class BaseQuerySet(object):
         Must handle requests to next pages in SWAPI when objects in the current
         page were all consumed.
         """
-        
+
         while True:
             if self.collected == self.total_records:
                 raise StopIteration
@@ -117,55 +87,19 @@ class BaseQuerySet(object):
                 self.records = page_data['results']
                 self.counter = 0
 
-            # this has to be a total hack.... but it works for now
-            # before, we manually referenced elem = People(self.records[self.counter])
-            # however, "self" here is PeopleQueryset. If we can somehow obtain "People" from "self",
-            # then we can replace the hack in the next line
-            elem = eval((self.RESOURCE_NAME).capitalize())(self.records[self.counter])
+            elem = eval((self.RESOURCE_NAME).capitalize())(
+                self.records[self.counter])
 
             self.counter += 1
             self.collected += 1
             return elem
 
-                
-#            elem = eval(self.__class__.__name__)()
-            # print(type(elem))
-            # print(elem)
-            # print('-'*20)
-#            elem = eval(self.__class__.__name__)(self.records[self.counter])
-#                elem = eval(self.__class__.__name__)(self.records[self.counter]) # TypeError: __init__() takes exactly 1 argument (2 given)
-#            elem = eval(self.__class__.__name__)((self.records[self.counter])) # TypeError: __init__() takes exactly 1 argument (2 given)
-#            print(type(self._get_page_data()))
-
-#            e2 = eval(self.__class__.__name__)()
-#            print(e3)
-#            print(type(e2))
-#            print(dir(e2))
-#            print('-'*20)
-
-            # # this is what we had before
-            # if self.RESOURCE_NAME == "people":
-            #     elem = People(self.records[self.counter])
-            # if self.RESOURCE_NAME == "films":
-            #     elem = Films(self.records[self.counter])
-
-            # self.counter += 1
-            # self.collected += 1
-            # return elem
-
     next = __next__
 
     def _get_page_data(self, page_number=1):
-        
+
         # made more general
         return getattr(api_client, "get_{}".format(self.RESOURCE_NAME))(page=page_number)
-
-        # # previously
-        # if self.RESOURCE_NAME == 'people':
-        #     json_data = api_client.get_people(page=page_number)
-        # if self.RESOURCE_NAME == 'films':
-        #     json_data = api_client.get_films(page=page_number)
-        # return json_data
 
     def count(self):
         """
